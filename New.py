9964a7b0799a0093b40335b500719432b7ab9f11
VadimCine <aka-sem@yandex.ru>
2024-08-20 23:18:02 +0300

    Add files via upload

---
+++ New.py
@@ -0,0 +1,18 @@
+# 1st program
+print(9**0.5 * 5)
+
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+print(2*2)
+print(2 * (2+2))
+print((2+2) == (2 * (2+2))) #либо print(4 == 8)
+
+#4rd program
+print('123.456')
+print(type(float('123.456')))
+print(123.456 * 10)
+print(int(1234))
+print(1234 / 10)
+print(int((123.4 - 123) * 10))
